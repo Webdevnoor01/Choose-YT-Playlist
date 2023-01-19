f6de87ee271054d42cdceb99fcb5aa2003c09c09
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2023-01-19 09:44:43 +0530

    Simplify the logic of usePlaylistHook after adding redux to manage global state.

--- src/hooks/usePlaylists.js
+++ src/hooks/usePlaylists.js
@@ -1,81 +1,55 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import getPlaylist from "../api";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPlaylist } from "../store/playlistSlice";
 import storage from "../utils/Storage";
+import { setplaylist } from "../store/playlistSlice";
+import { INIT_STATE } from "../store/playlistSlice";
+const STORAGE_KEY = "C-YT-PLAYLIST";
 
-const STORAGE_KEY = "C-YT-PLAYLIST"
-const INIT_STATE = {
-  playlists: {},
-  favoritesPlaylists: [],
-  recentPlaylists: [],
-}
 const usePlaylists = () => {
-  const [state, setState] = useState(INIT_STATE);
-  const [error, setError] = useState("")
-  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+  const { playlists, error, loading } =state.playlist
 
-  useEffect(() =>{
-    const data = storage.get(STORAGE_KEY)
-    if(data) {
-      setState({...data})
+  useEffect(() => {
+    const data = storage.get(STORAGE_KEY);
+    console.log(data)
+    if (data) {
+      dispatch(setplaylist(data))
+      console.log(data);
     }
-  },[])
+    
+  }, []);
 
-  useEffect(()=>{
-    if(state !== INIT_STATE){
-      storage.save(STORAGE_KEY, state)
+  useEffect(() => {
+    console.log(state.playlist===INIT_STATE)
+    if (state.playlist !== INIT_STATE) {
+      storage.save(STORAGE_KEY, state.playlist);
+      console.log("save state to storage", state.playlist)
     }
-  },[state])
+  }, [state]);
 
   const getPlaylistsById = async (playlistId) => {
-    if (state.playlists[playlistId]) {
-      // return state.playlists;
-      const videosLen = state.playlists[playlistId].videos.length
-      console.log(videosLen)
-      setLoading(true)
-      try{
-       const result = await getPlaylist(playlistId);
-       console.log(result)
-        setError('')
-        if(videosLen === result.videos.length) return state.playlists
-        setState(prev =>({
-          ...prev,
-          playlists:{
-            ...prev.playlists,
-            [playlistId]:result
-          }
-        }))
-      }catch (e) {
-        console.log(e)
-        setError(e?.response?.data?.error?.message || "Something went wrong")
-      }finally{
-        setLoading(false)
-      }
-    }
-
-  
+    dispatch(fetchPlaylist(playlistId));
   };
 
-  const addToFavorite = (playListId) => {
-    setState((prev) => ({
-      favoritesPlaylists: [...prev.favoritesPlaylists, playListId],
-    }));
-  };
+  // const addToFavorite = (playListId) => {
+  //   setState((prev) => ({
+  //     favoritesPlaylists: [...prev.favoritesPlaylists, playListId],
+  //   }));
+  // };
 
-  const addToRecent = (playListId) => {
-    setState((prev) => ({
-      recentPlaylists: [...prev.recentPlaylists, playListId],
-    }));
-  };
-
+  // const addToRecent = (playListId) => {
+  //   setState((prev) => ({
+  //     recentPlaylists: [...prev.recentPlaylists, playListId],
+  //   }));
+  // };
 
   return {
-    playlists: state.playlists,
+    playlists,
     getPlaylistsById,
-    addToFavorite,
-    addToRecent,
     error,
-    loading
+    loading,
   };
 };
 
