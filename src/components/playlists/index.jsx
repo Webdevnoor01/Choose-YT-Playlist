39aa838992eca53734def6315228f596a97b28e9
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2023-01-19 09:39:39 +0530

    Update playlist card component.

--- src/components/playlists/index.jsx
+++ src/components/playlists/index.jsx
@@ -1,21 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux"
 import PlaylistCard from "../playlist-card";
 import Grid from "@mui/material/Grid";
+import { useParams } from "react-router-dom";
 
-const Playlists = ({ playlists }) => {
-  const playlistArr = Object.values(playlists);
+
+const Playlists = () => {
+  const state = useSelector(state => state.playlist)
+  const playlistArr =  Object.values(state.playlists);
   return (
     <>
       <Grid container alignItems={"stretch"}  sx={{maxWidth:"80%", margin:"0 auto"}} >
         {playlistArr.length > 0 &&
           playlistArr.map((playlist) => (
-            <Grid item alignItems={"stretch"}  sx={4} >
+            <Grid key={playlist.playlistId} item alignItems={"stretch"}  sx={4} >
               <PlaylistCard
                 ChannelTitle={playlist.channelTitle}
                 playlistTitle={playlist.playlistTitle}
                 thumbnail={playlist.playlistThumbnail}
                 playlistId={playlist.playlistId}
                 key={playlist.playlistId}
+                videoId={playlist.videos[0].videoContentDetails.videoId}
               />
             </Grid>
           ))}
