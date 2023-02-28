e1dc87b66b531f9c795cec101bd35a6d6671f8eb
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2023-02-28 20:51:17 +0530

    resolve merge conflict.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -41,32 +40,5 @@ function App() {
     </ThemeProvider>
   );
 }
-=======
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CssBaseline from "@mui/material/CssBaseline";
-import NavBar from "./components/navbar/index";
-import usePlaylist from "./hooks/usePlaylists";
-import Playlists from "./components/playlists";
-import Player from "./pages/player-page";
-import "./app.css"
-
-const App = () => {
-  const { playlists, error, getPlaylistsById } = usePlaylist();
-  console
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <CssBaseline />
-        <NavBar getPlayListById={getPlaylistsById} />
-        <Routes>
-          <Route path="/" element={<Playlists/>} />
-          <Route path="/playlist/:playlistId/watch" element={<Player />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-};
->>>>>>> main
 
 export default App;
